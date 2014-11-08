4e6580fcbb4d1e4eb576f83d7ef60b6c90f4095b
Tristan Slater <tristan@kanso.ca>
2014-11-08 01:38:21 -0800

    Switched to simple settings page

--- ldca-settings.php
+++ ldca-settings.php
@@ -18,28 +18,10 @@ add_action('admin_menu','ldca_settings');
  * @return null
  */
 function ldca_settings_init() {
-  
   register_setting(
     'ldca_settings_general',
     'store_url',
     'esc_url_raw'
-  );
-  
-  // Attach general section to general page
-  add_settings_section(
-    'ldca-settings-section-all',
-    '',
-    'ldca_settings_section_all_cb',
-    'ldca-settings-page-general'
-  );
-  
-  // Add field to section
-  add_settings_field(
-    'ldca-settings-field-store-url',
-    'Store URL',
-    'ldca_settings_field_store_url_cb',
-    'ldca-settings-page-general',
-    'ldca-settings-section-all'
   );
 }
 add_action('admin_init','ldca_settings_init');
@@ -53,6 +35,8 @@ add_action('admin_init','ldca_settings_init');
  */
 function ldca_settings_page_general_cb() {
   
+  $store_url_val = esc_attr(get_option('store_url'));
+  
   // HTML Output
   ?>
   
@@ -60,48 +44,23 @@ function ldca_settings_page_general_cb() {
     <h2>LearnDash Course Activator Settings</h2>
     
     <form method="post" action="options.php">
-      <?php
+      <?php settings_fields('ldca_settings_general'); ?>
       
-      // Output section, defined here: ldca_settings_section_all_cb
-      settings_fields('ldca_settings_general');
-      
-      // Output fields, defined here: ldca_settings_field_store_url_cb
-      do_settings_sections('ldca-settings-page-general');
-      
-      submit_button();
-      
-      ?>
+      <table class="form-table">
+        <tbody>
+          <tr>
+            <th scope="row">Store URL</th>
+            
+            <td>
+              <input type="text" name="store_url" value="<?php echo $store_url_val; ?>" size="40">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  
+      <?php submit_button(); ?>
     </form>
   </div>
-  
-  <?php
-}
-
-
-
-/**
- * Sets up the section to hold the fields
- * 
- * @return null
- */
-function ldca_settings_section_all_cb() {
-  // Empty...
-}
-
-
-
-/**
- * Handles logic and HTML output for store URL field
- * 
- * @return null
- */
-function ldca_settings_field_store_url_cb() {
-  
-  $val = esc_attr(get_option('store_url'));
-  
-  ?>
-  
-  <input type="text" name="store_url" value="<?php echo $val; ?>" size="40">
   
   <?php
 }
